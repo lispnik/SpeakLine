18e50c405ded1dda178f37eaeacb92a7dec42e29
Matthew Kennedy <burnsidemk@gmail.com>
2020-04-22 18:57:51 -0500

    Initial Commit

---
+++ SpeakLine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpeakLine
+//
+//  Created by Matthew Kennedy on 4/22/20.
+//  Copyright © 2020 Matthew Kennedy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
